79063487d53265cf49935da608381fc47309c9b9
Suma <75930308+sumamukkamala2310@users.noreply.github.com>
2021-03-18 19:08:32 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,1 +1,14 @@
-//WAP to find the sum of n fractions.
+#include<stdio.h>
+int main()
+{
+int n, i, sum;
+ int sum=0;
+printf("Enter a positive integer:");
+scanf("%d", &n);
+for(i = 1; i<= n; i++)
+{
+sum += 1;
+}
+printf("Sum = %d", sum);
+return 0;
+} 
